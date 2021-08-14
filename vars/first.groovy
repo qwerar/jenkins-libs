6581ea27c12f7d2353923f6c0c0cae2f3939aeff
qwerar <qwerar@gmail.com>
2021-08-14 14:37:42 +0300

    Update first.groovy

--- vars/first.groovy
+++ vars/first.groovy
@@ -1,3 +1,9 @@
-def call(String name = "default"){
+def name(String name = "default"){
 echo "Hello this is ${name}"
+}
+def sum(x,y){
+  echo "Som of ${x}+${y} is ${x+y}"
+}
+def mult(x,y){
+  echo "Multiply result of ${x} and ${y} is ${x*y} "
 }
